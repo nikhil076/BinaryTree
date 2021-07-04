2f12e08c7493e99e366bca164d6f85a72a083757
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-04 22:36:28 +0530

    Initialized the program

---
+++ src/com/bridgelabz/binary/BinaryNode.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.binary;
+
+public class BinaryNode<T extends Comparable<T>> 
+{
+	
+	T key;
+	BinaryNode<T> left;
+	BinaryNode<T> right;
+	
+	public BinaryNode(T key)
+	{
+		super();
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+
+	public BinaryNode() 
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "BinaryNode [data=" + key + ", left=" + left + ", right=" + right + "]";
+	}
+
+
+}
---
+++ src/com/bridgelabz/binary/MyBinary.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.binary;
+
+public class MyBinary<K extends Comparable<K>>
+{	
+	private BinaryNode<K> root;
+	
+	
+	public void add(K key)
+	{
+		this.root = this.insertData(root,key);
+	}
+
+	public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+	{
+		if(current ==  null)
+			return new BinaryNode<>(key);
+		int compareResult = key.compareTo(current.key);
+		if(compareResult==0) return current;
+		if(compareResult<0)
+		{
+			current.left = insertData(current.left, key);
+		}
+		else 
+		{
+			current.right = insertData(current.right, key);
+		}
+		return current;
+	}
+	@Override
+	public String toString() {
+		return "MyBinary [root=" + root + "]";
+	}
+
+	public MyBinary() 
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		MyBinary<Integer> binaryTree = new MyBinary<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		System.out.println(binaryTree);
+	}
+
+}
